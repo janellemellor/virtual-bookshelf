b8651b2a7de8f33fcb8beeb85ce48c77f5b40e32
janelle mellor <janellemellor@gmail.com>
2020-05-13 16:45:30 -0700

    add local form input state to provider

--- src/hooks/BookProvider.jsx
+++ src/hooks/BookProvider.jsx
@@ -1,9 +1,15 @@
 import React, { createContext, useState, useContext } from "react";
  
+const BookContext = createContext();
 
-export const BookProvider = ({}) => {
+export const BookProvider = ({ children }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [genre, setGenre] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [synopsis, setSynopsis] = useState('');
+
 
 };
 
 
-//onSubmit, onChange, title, author, genre, imageUrl, synopsis
