8b6a954d2fb310d59bce5b61c3f417f0668e52e6
janelle mellor <janellemellor@gmail.com>
2020-05-13 17:58:43 -0700

    add handler for add book action to reducer

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,5 +1,9 @@
+import { ADD_BOOK } from "../actions/bookActions";
+
 export default function reducer(state = [], action) {
     switch(action.type) {
+      case ADD_BOOK:
+        return [...state, action.payload];
       default:
         return state;
     }
